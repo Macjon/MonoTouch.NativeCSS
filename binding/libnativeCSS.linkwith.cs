2da5ae38bdf1b839cfa1b39b5fb6f5163385843f
Jonah Hulselmans <jonah.hulselmans@flowpilots.com>
2013-11-29 18:41:52 +0100

    Added method to style application and any new views with CSS from a remote URL.
    Style application and any new views with CSS from a remote URL.
     On the first load the initial CSS file will be used, whilst the remote
    CSS and its assets are downloaded.

--- binding/libnativeCSS.linkwith.cs
+++ binding/libnativeCSS.linkwith.cs
@@ -1,5 +1,5 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libnativeCSS.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true)] 
+[assembly: LinkWith("libnativeCSS.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true, Frameworks = "QuartzCore, CoreImage, Accelerate, CoreText", LinkerFlags = "-lz -all_load -ObjC")]
 
--- sample/AppDelegate.cs
+++ sample/AppDelegate.cs
@@ -3,42 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using MonoTouch.NativeCSS;
 
-namespace Samples {
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate {
-		private UIWindow window;
-		private MyViewController viewController;
+namespace Samples
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        private UIWindow window;
+        private MyViewController viewController;
 
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-			viewController = new MyViewController ();
-			window.RootViewController = viewController;
+            viewController = new MyViewController();
+            window.RootViewController = viewController;
 
-			window.MakeKeyAndVisible ();
+            window.MakeKeyAndVisible();
 
-			// NativeCSS is required one license per each bundle id.
+            // NativeCSS is required one license per each bundle id.
 
-			// sample
-			MonoTouch.NativeCSS.CSSUIApplication.StyleWithCSSString("button { background-color: #a1a1a1; }");
+            // sample
+            //http://localhost:8000/styles.css
+            // To live edit, cd to styles.css and run "python -m SimpleHTTPServer"
+            var url = new NSUrl("http://macjon.local:8000/styles.css");
+            CSSUIApplication.StyleWithCSSFileAndUrl("styles.css", url, 1);
+//            CSSUIApplication.UpdateCSSFromURL(url, 1, ((bool success, bool cssIsDifferent, string cssContent) =>
+//            {
+//                Console.WriteLine("Succes: {0}\nCssIsDifferent: {1}\nCssContent: {2}", success, cssIsDifferent, cssContent);
+//            }));
 
-			// sample
-			//MonoTouch.NativeCSS.CSSUIApplication.UpdateCSSFromURL(new NSUrl("http", "your ip address", "/path/filename.css"), 3, delegate(bool success, bool different, string content)
-			//{
-			//	if (success && different) {
-			//		MonoTouch.NativeCSS.CSSUIApplication.StyleWithCSSString(content);
-			//	} else if (!success) {
-			//		MonoTouch.NativeCSS.CSSUIApplication.StyleWithCSSString("button { background-color: #a1a1a1; }");
-			//	}
-			//});
+//            CSSUIApplication.StyleWithCSSString("button { background-color: #ff0000; }");
 
-			// sample
-			//var myHandler = new MonoTouch.NativeCSS.UpdateCSSFromURLHandler((bool success, bool different, string content) => { if (success) { } });
-			//MonoTouch.NativeCSS.CSSUIApplication.UpdateCSSFromURL(new NSUrl("http", "your ip address", "/path/filename.css"), myHandler);
+            // sample
+            //MonoTouch.NativeCSS.CSSUIApplication.UpdateCSSFromURL(new NSUrl("http", "your ip address", "/path/filename.css"), 3, delegate(bool success, bool different, string content)
+            //{
+            //	if (success && different) {
+            //		MonoTouch.NativeCSS.CSSUIApplication.StyleWithCSSString(content);
+            //	} else if (!success) {
+            //		MonoTouch.NativeCSS.CSSUIApplication.StyleWithCSSString("button { background-color: #a1a1a1; }");
+            //	}
+            //});
 
-			return true;
-		}
-	}
+            // sample
+            //var myHandler = new MonoTouch.NativeCSS.UpdateCSSFromURLHandler((bool success, bool different, string content) => { if (success) { } });
+            //MonoTouch.NativeCSS.CSSUIApplication.UpdateCSSFromURL(new NSUrl("http", "your ip address", "/path/filename.css"), myHandler);
+
+            return true;
+        }
+    }
 }
